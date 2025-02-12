a35ed36d6e4dd814448e90ca9b1b8d1864b907f1
Andika Palian <andika12des@gmail.com>
2025-02-12 16:38:48 +0800

    feat: restrict admin registration to super admins with validation middleware

--- src/routes/admin.routes.js
+++ src/routes/admin.routes.js
@@ -1,9 +1,12 @@
 import express from "express";
 import { loginAdmin, registerAdmin } from "../controllers/admin.controllers.js";
+import { adminValidation, hashRole } from "../middlewares/adminValidation.js";
 
 const adminRouter = express.Router();
 
-adminRouter.post("/register", registerAdmin);
+// Only super admins can register other admins
+adminRouter.post("/register", adminValidation, hashRole(["SUPER_ADMIN"]), registerAdmin);
+
 adminRouter.post("/login", loginAdmin);
 
 export default adminRouter;
